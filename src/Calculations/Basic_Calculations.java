0c98d17ac426ab1ba5e63617a2097a372c840a49
HandySammyDev <lsamuelwell@gmail.com>
2025-05-28 12:27:44 -0400

    Add files via upload
    Signed-off-by: HandySammyDev <lsamuelwell@gmail.com>

---
+++ src/Calculations/Basic_Calculations.java
@@ -0,0 +1,33 @@
+package Test8.Calculations;
+
+import Test8.TopPanel;
+
+public class Basic_Calculations {
+    TopPanel topPanel = new TopPanel();
+
+    String text;
+    public String getText(){
+        text = topPanel.getActiveTextField().getText();
+        return text;
+    }
+
+    public void adding(){
+
+    }
+    public void subtracting(){
+
+    }
+    public void multiplying(){
+
+    }
+    public void dividing(){
+
+    }
+    public void ans(){
+
+    }
+
+    Basic_Calculations(){
+
+    }
+}

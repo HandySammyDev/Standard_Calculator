a55b597945881441eab54d8a0489c2f645f67031
HandySammyDev <lsamuelwell@gmail.com>
2025-05-29 17:53:37 -0400

    Add files via upload
    Signed-off-by: HandySammyDev <lsamuelwell@gmail.com>

--- src/Calculations/Basic_Calculations.java
+++ src/Calculations/Basic_Calculations.java
@@ -1,6 +1,9 @@
 package Test8.Calculations;
 
 public class Basic_Calculations{
+    private String text;
+    private int number;
+
     public void adding(){
 
     }
@@ -17,7 +20,23 @@ public class Basic_Calculations{
 
     }
 
+    public double convertTextToDouble(String text){
+        try{
+            return Double.parseDouble(text);
+        }
+        catch(NumberFormatException e){
+            System.out.println(e.getMessage());
+            return 0.00000;
+        }
+    }
+
     public Basic_Calculations(String text){
-        System.out.println(text);
+        this.text = text;
+        System.out.println("String: " + text);
+        System.out.println(("Number: " + convertTextToDouble(getText())));
+    }
+
+    public String getText() {
+        return text;
     }
 }

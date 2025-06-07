4bf8fd515e9f238e1c355ff849981d463021dd5f
HandySammyDev <lsamuelwell@gmail.com>
2025-06-06 21:31:09 -0400

    Add files via upload
    Signed-off-by: HandySammyDev <lsamuelwell@gmail.com>

--- src/Calculations/Basic_Calculations.java
+++ src/Calculations/Basic_Calculations.java
@@ -1,5 +1,6 @@
-package Test9.Calculations;
+package Calculations;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class Basic_Calculations{
@@ -7,30 +8,47 @@ public class Basic_Calculations{
 
     public void RPN_method(){
         Stack<Double> numberStack = new Stack<>();
-
         String numberString = "";
-        double number = 0.0;
+        char highestOperator = ' ';
+
+        double num1;
+        double num2;
 
         for(int i=0; i<equation.length(); i++){
             if(isOperator(equation.charAt(i))){
+                System.out.println("New number: " + numberString);
+                numberStack.push(convertToDouble(numberString));
+                highestOperator = equation.charAt(i);
+
                 numberString = "";
                 continue;
             }
 
             if(Character.isDigit(equation.charAt(i))){
                 numberString += equation.charAt(i);
-                System.out.println(numberString);
             }
+        }
+        System.out.println("New number: " + numberString);
+        numberStack.push(convertToDouble(numberString));
+        System.out.println(numberStack);
 
-            try{
-                number = Double.parseDouble(numberString);
-            }
-            catch(NumberFormatException e){
-                System.out.println("Error");
-                return;
-            }
+        //2+3*4
+        if(highestOperator == '*'){
+            num1 = numberStack.pop();
+            num2 = numberStack.pop();
+            numberStack.push(num1 * num2);
 
-            numberStack.push(number);
+            System.out.println(num1 * num2);
+        }
+    }
+
+    public double convertToDouble(String string){
+        try{
+            return Double.parseDouble(string);
+        }
+        catch(NumberFormatException e){
+            System.out.println("Error");
+            return -1;
         }
     }
 

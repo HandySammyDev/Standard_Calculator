a66631ff650b190344224bd6081f700cbc63856e
HandySammyDev <lsamuelwell@gmail.com>
2025-06-30 20:31:13 -0400

    Add files via upload
    Signed-off-by: HandySammyDev <lsamuelwell@gmail.com>

--- src/BottomPanel.java
+++ src/BottomPanel.java
@@ -7,19 +7,44 @@ import java.awt.event.ActionListener;
 
 public class BottomPanel extends JPanel implements ActionListener{
     String[] buttonsArr =
-            {"π","1","2","3","+","clear",
-            "%", "4","5","6","-","<--",
-            "|a|","7","8","9","x"," ",
-            "(", "0", ".","ans","/","<--|"
+            {"π","1","2","3","+",//"clear",
+            "%", "4","5","6","-",//"<--",
+            "|a|","7","8","9","x",//" ",
+            "(", "0", ".","ans","/",//"<--|"
+            "undo", "redo", "clear","<--","<--|"
             };
 
     public void createButtons(){
         for (String s : buttonsArr) {
-            String fontColor = "#59a6c0";
-            if(s.equals("+") || s.equals("-") || s.equals("x") || s.equals("/")){
+            String fontColor = "#217c7d";//"#59a6c0";
+            String colorGradient1 = "#31383d";
+            String colorGradient2 = "#121317";
+            String colorHover1 = "#4b5359";
+            String colorHover2 = "#2b2c31";
+            String colorPressed1 = "#657078";
+            String colorPressed2 = "#43454c";
+
+            if(s.equals("+") || s.equals("-") || s.equals("x") || s.equals("/") ||
+                s.equals("undo") || s.equals("redo") || s.equals("clear") || s.equals("<--")){
                 fontColor = "#b55968";
             }
-            JButton button = new GradientButton(s, fontColor, "#31383d", "#121317");
+            if(s.equals("<--|") || s.equals("ans")){
+                fontColor = "#eeeeee";
+                colorGradient1 = "#055f60";
+                colorGradient2 = "#033f40";
+                colorHover1 = "#217c7d";
+                colorHover2 = "#1f5d5e";
+                colorPressed1 = "#3a999a";
+                colorPressed2 = "#3b7b7c";
+            }
+
+            JButton button = new GradientButton
+                    (s,
+                    fontColor,
+                    colorGradient1, colorGradient2,
+                    colorHover1, colorHover2,
+                    colorPressed1, colorPressed2);
+
             button.setActionCommand(s);
             button.addActionListener(this);
             this.add(button);
@@ -29,7 +54,7 @@ public class BottomPanel extends JPanel implements ActionListener{
     BottomPanel(){
         final int hGap = 4;
         final int vGap = 4;
-        this.setLayout(new GridLayout(4,6,hGap,vGap));
+        this.setLayout(new GridLayout(5,5,hGap,vGap));
         this.setBackground(Color.decode("#1d2226"));
         createButtons();
     }
--- src/CustomButtonDesign/GradientButton.java
+++ src/CustomButtonDesign/GradientButton.java
@@ -2,10 +2,19 @@ package CustomButtonDesign;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.net.CookieHandler;
 
 public class GradientButton extends JButton {
+    private boolean isHovered = true;
+    private boolean isPressed = false;
     String colorGradient1;
     String colorGradient2;
+    String colorHover1;
+    String colorHover2;
+    String colorPressed1;
+    String colorPressed2;
 
     private String getColorGradient1(){
         return colorGradient1;
@@ -13,17 +22,65 @@ public class GradientButton extends JButton {
     private String getColorGradient2(){
         return colorGradient2;
     }
+    private String getColorHover1(){
+        return colorHover1;
+    }
+    private String getColorHover2(){
+        return colorHover2;
+    }
+    private String getColorPressed1(){
+        return colorPressed1;
+    }
+    private String getColorPressed2(){
+        return colorPressed2;
+    }
 
-    public GradientButton(String text, String fontColor, String colorGradient1, String colorGradient2){
+    public GradientButton
+            (String text,
+             String fontColor,
+             String colorGradient1, String colorGradient2,
+             String colorHover1, String colorHover2,
+             String colorPressed1, String colorPressed2){
         super(text);
         setContentAreaFilled(false); //Allows custom painting
-        setForeground(Color.decode(fontColor)); //text color
-        setFont(new Font("Arial", Font.BOLD,15));
         setFocusPainted(false);
         setBorderPainted(false);
 
+        setForeground(Color.decode(fontColor)); //text color
+        setFont(new Font("Arial", Font.BOLD,15));
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseExited(MouseEvent e) {
+                isHovered = true;
+                repaint();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                isHovered = false;
+                repaint();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                isPressed = true;
+                repaint();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                isPressed = false;
+                repaint();
+            }
+        });
+
         this.colorGradient1 = colorGradient1;
         this.colorGradient2 = colorGradient2;
+        this.colorHover1 = colorHover1;
+        this.colorHover2 = colorHover2;
+        this.colorPressed1 = colorPressed1;
+        this.colorPressed2 = colorPressed2;
     }
 
     protected void paintComponent(Graphics g){
@@ -48,14 +105,27 @@ public class GradientButton extends JButton {
         g2.fillRoundRect(6,6,width,height,arc,arc);
 
         //Create gradient paint (from top to bottom)
-        GradientPaint gp = new GradientPaint(
-                0,0,Color.decode(getColorGradient1()), //Top color "#31383d"
-                0, getHeight(), Color.decode(getColorGradient2()) //Bottom color "#121317"
-        );
+        GradientPaint gradient;
+
+        if(isPressed){
+            gradient = new GradientPaint(
+            0,0, Color.decode(getColorPressed1()),
+            0, height, Color.decode(getColorPressed2()));
+        }
+        else if(isHovered){
+            gradient = new GradientPaint(
+            0,0,Color.decode(getColorGradient1()), //Top color
+            0, height, Color.decode(getColorGradient2())); //Bottom color
+        }
+        else{
+            gradient = new GradientPaint(
+            0,0, Color.decode(getColorHover1()),
+            0, height, Color.decode(getColorHover2()));
+        }
 
         //Fill with gradient
-        g2.setPaint(gp);
-        g2.fillRoundRect(0,0,getWidth(),getHeight(),10,10);
+        g2.setPaint(gradient);
+        g2.fillRoundRect(0,0,width,height,10,10);
 
         g2.dispose(); //Clean up
 
